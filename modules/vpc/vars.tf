b317aaac53b3b8b036eac2bd1149cd040d856d75
unknown <lzaa.dev@outlook.com>
2020-07-19 13:51:02 +0800

    fixed format

--- modules/vpc/vars.tf
+++ modules/vpc/vars.tf
@@ -1,40 +1,52 @@
 ### General Variables ###
+variable "name" {
+  description = "Prefix that will be added to all resources as identifier."
+  type        = string
+  default     = ""
+}
+
 variable "my_ip" {
   description = "Your work, home, internal IP that will be use for example SSH source."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
+
 variable "default_tags" {
   description = "Default tags that are added to every created resource."
-  type = map
-  default = {
+  type        = map
+  default     = {
     Terraform = "True"
   }
 }
+
 variable "global_tags" {
   description = "Global tags that will be applied to all of the resources created using this module."
-  type = map
-  default = {}
+  type        = map
+  default     = {}
 }
 
 ### VPC Variables ###
 variable "aws_region" {
   description = "AWS Region"
-  type = string
+  type        = string
 }
+
 variable "vpc_cidr_block" {
   description = "VPC CIDR block"
-  type = string
+  type        = string
 }
+
 variable "azs" {
   description = "List of availability zone that will be used."
-  type = list
+  type        = list
 }
+
 variable "enable_dns_support" {
   description = "Enable or disable DNS support in the VPC. Default true."
   type        = bool
   default     = true
 }
+
 variable "enable_dns_hostnames" {
   description = "Enable or disable DNS hostname in the VPC. Default true."
   type        = bool
@@ -44,51 +56,56 @@ variable "enable_dns_hostnames" {
 ### NAT Gateway Variables ###
 variable "enable_nat_gateway"{
   description = "Flag whether to create NAT Gateway or not."
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 ### Subnets Variables ###
 variable "public_subnets" {
   description = "Public subnets with route to the Internet and map_public_ip_on_launch set to true."
-  type = list
-  default = []
+  type        = list
+  default     = []
 }
+
 variable "intra_subnets" {
   description = "Private subnets that have no Internet routing."
-  type = list
-  default = []
+  type        = list
+  default     = []
 }
+
 variable "private_subnets" {
   description = "Private subnets that have Internet routing through NAT gateway."
-  type = list
-  default = []
+  type        = list
+  default     = []
 }
+
 variable "db_subnets" {
   description = "Private subnets for database that have Internet routing through NAT gateway."
-  type = list
-  default = []
+  type        = list
+  default     = []
 }
 
 ### NACL Rules Variables ###
 variable "public_nacl_rules" {
   description = "Network ACL rules for public subnets."
-  type = map
-  default = {
+  type        = map
+  default     = {
     # <rule_no> = [<from_port>, <to_port>, <protocol>, <cidr_block>, <action>],
   }
 }
+
 variable "private_nacl_rules" {
   description = "Network ACL rules for private subnets."
-  type = map
-  default = {
+  type        = map
+  default     = {
     # <rule_no> = [<from_port>, <to_port>, <protocol>, <cidr_block>, <action>],
   }
 }
+
 variable "db_nacl_rules" {
   description = "Network ACL rules for database subnets."
-  type = map
-  default = {
+  type        = map
+  default     = {
     # <rule_no> = [<from_port>, <to_port>, <protocol>, <cidr_block>, <action>],
   }
 }
